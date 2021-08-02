bfa2ef798f8b749ea11a50ea06aad3afa1bd8297
Touhid <aptounid6@gmail.com>
2021-08-03 01:45:39 +0600

    Templating done

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('admin.layouts._head')
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{asset('assets/admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+  <!-- Navbar -->
+  @include('admin.layouts._topNav')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+
+    @include('admin.layouts._leftNav')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+      @yield('content')
+  </div>
+  <!-- /.content-wrapper -->
+  <!-- footer -->
+      @include('admin.layouts._footer')
+  <!-- footer -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+    @include('admin.layouts._js')
+<!-- jQuery -->
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/dashboard', function (){
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+Route::get('admin/blogs', function (){
+    return view('admin.blog');
+});
