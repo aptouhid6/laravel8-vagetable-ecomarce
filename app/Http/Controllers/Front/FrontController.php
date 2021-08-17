3af890db087a2241ba261139864f35a244575928
Touhid <aptounid6@gmail.com>
2021-08-18 01:54:35 +0600

    Featured product show in front

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class FrontController extends Controller
 {
     public function home(){
         // dd('yes');
-        return view('front.home');
+        $data['featured_products'] = Product::where('is_featured',1)->limit(8)->get();
+        return view('front.home',$data);
     }
 }
